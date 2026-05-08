106a5f586dde7e089f9e98705d74bbd2f10a5723
Zac Vukovic <26176372+cyn1x@users.noreply.github.com>
2026-05-08 17:16:44 +1000

    Tidy `build.bat` and remove log.c depdendency

--- tools/build.bat
+++ tools/build.bat
@@ -1,100 +1,175 @@
+:: TODO
+
+:: MSVC Compiler and Linker Options
+:: https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-alphabetically?view=msvc-170
+:: https://learn.microsoft.com/en-us/cpp/build/reference/linker-options?view=msvc-170
+
 @echo off
 
 setlocal enabledelayedexpansion
 
-set err=
+set Err=
 
-set argCount=0
-for %%x in (%*) do (
-    set /A argCount+=1
-    set "argVec[!argCount!]=%%~x"
+set ArgCount=0
+for %%I in (%*) do (
+    set /A ArgCount+=1
+    set "ArgVec[!ArgCount!]=%%~I"
 )
 
 rem Default fallback if MSVC does not automatically set platform
-if "%platform%"=="" set platform=x64
+if "%Platform%"=="" set Platform=x64
 
-set config=Debug
-set compileropts=-DDEBUG=1
-set linkeropts=/DEBUG
+rem Default build configuration
+set Config=Debug
+set CompilerOpts=-DDEBUG=1
+set LinkerOpts=/DEBUG
 
 rem Set project root directory
-set root=%~dp0..
-for %%I in ("%ROOT%") do set "root=%%~fI"
+set Root=%~dp0..
+for %%I in ("%Root%") do set "Root=%%~fI"
 
 rem Process command-line arguments arguments
-for /L %%i in (1,1,%argCount%) do (
+for /L %%I in (1,1,%ArgCount%) do (
     
-    set arg=!argVec[%%i]!
+    set arg=!argVec[%%I]!
 
-    if !arg! EQU clean goto :clean
+    if !Arg! EQU clean goto :clean
 
-    if !arg! EQU release (
-        set compileropts=-Od -DDEBUG=0
-        set linkeropts=/OPT:REF
-        set config=Release
+    if !Arg! EQU release (
+        set CompilerOpts=-Od -DDEBUG=0
+        set LinkerOpts=/OPT:REF
+        set Config=Release
     )
-    if !arg! EQU x86 (
+    if !Arg! EQU x86 (
         rem Override platform set by MSVC if specified via command-line args
-        set platform=x86
+        set Platform=x86
     )
 
 )
 
 rem End of entry point procedure
-goto :main
+goto :Main
 
-rem ==========================
-rem Clean
-rem ==========================
-:clean
-rem Deletes all intermediary and binary files from previous compilations
-del /S /Q bin\*.dll bin\*.exe bin\*.lib bin\*.pdb bin\*.ilk bin\*.exp > nul 2>&1
-del /S /Q obj\*.obj obj\*.pdb > nul 2>&1
-del /S /Q tests\obj\*.obj tests\obj\*.pdb > nul 2>&1 
-del /S /Q tests\bin\*.dll tests\bin\*.exe tests\bin\*.lib tests\bin\*.pdb^
-    tests\bin\*.ilk tests\bin\*.exp > nul 2>&1
 
-rem End of :clean subroutine call
-goto :eof
+::-----------------------------------------------------------------------------
+:: Main
+::-----------------------------------------------------------------------------
+rem Main procedure that configurues the script environment and LSP compile flags
+:Main
 
-rem ==========================
-rem Main
-rem ==========================
-:main
-
+rem Configure development environment
 if not defined DevEnvDir (
     call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 )
 
 rem Define the engine dynamic link library and application executable filenames
-set _dll=libcspd_%platform%.dll
-set _lib=libcspd_%platform%.lib
-set _exe=libcspd_%platform%.exe
+set _dll=libcspd_%Platform%.dll
+set _lib=libcspd_%Platform%.lib
+set _exe=libcspd_%Platform%.exe
 
 rem Define `compile_flags.txt` file for the LLVM Clang LSP
-set flags=compile_flags.txt
+set CompileFlags=compile_flags.txt
 
 rem Clear `compile_flags.txt` file
-echo | set /p clear="" > %flags%
+echo | set /p Clear="" > %CompileFlags%
 
-rem Make required directories for core engine and sandbox environment
-call :mkdirs
-if %errorlevel% neq 0 goto :error
-
-goto :build
-
-rem ==========================
-rem Make Output Directories
-rem ==========================
-:mkdirs
-rem Make required directories for engine compilation
-
+rem Create required directories for library compilation
 if not exist obj mkdir obj
 if not exist obj\win32\%config%_x86 mkdir obj\win32\%config%_x86
 if not exist obj\win32\%config%_x64 mkdir obj\win32\%config%_x64
 if not exist bin mkdir bin
 if not exist bin\win32\%config%_x86 mkdir bin\win32\%config%_x86
 if not exist bin\win32\%config%_x64 mkdir bin\win32\%config%_x64
+if %ErrorLevel% neq 0 goto :error
+
+call :Build
+call :Test
+
+rem End of :Main subroutine call
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Build
+::-----------------------------------------------------------------------------
+rem Orchestrates the entire build process
+:Build
+
+rem Set the core library `include` and `src` directories
+set IncDir=%Root%\include
+set SrcDir=%Root%\src
+
+rem Set the core library `obj` and `bin` directories
+set ObjDir=%Root%\obj\win32\%Config%_%Platform%
+set BinDir=%Root%\bin\win32\%Config%_%Platform%
+
+rem Clear variables to store source, include, and object filenames.
+set Srcs=
+set Incs=
+set Objs=
+
+rem Reference Platform and Win32 header files
+call :IncludeDirs %IncDir%
+if %ErrorLevel% neq 0 goto :Error
+
+rem Reference Platform and Win32 source files
+call :SourceFiles %SrcDir%
+if %ErrorLevel% neq 0 goto :Error
+
+call :Compile
+
+rem End of :Build subroutine call
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Compile
+::-----------------------------------------------------------------------------
+rem Performs a compilation of source files and subsequent linking of the resulting object files
+:Compile
+
+
+rem Compile core library `*.c` files
+cl /c /MD -Zi -W4 -Wall /std:c17 /Fo%ObjDir%\ /Fd"%ObjDir%\vc140.pdb" %Incs% %Srcs%
+if %ErrorLevel% neq 0 goto :error
+echo Library source files compiled successfully & echo.
+
+call :ObjectFiles %ObjDir%
+if %ErrorLevel% neq 0 goto :Error
+
+rem Link *.obj object files
+LINK /nologo /DEBUG %Objs:~1% /DLL /OUT:%BinDir%\%_dll%
+if %ErrorLevel% neq 0 goto :Error
+
+call :RelativePath %BinDir%
+echo Library file `%_lib%` created in `%relpath%` successfully. & echo.
+
+rem Copy DLL to where the test executable will be built
+copy %bindir%\%_dll% %root%\tests\bin\win32\%config%_%platform% > nul
+
+rem End of :Compile subroutine
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Test
+::-----------------------------------------------------------------------------
+rem Build test suite
+:Test
+
+rem Set the core library `include` and `src` directories
+set IncDir=%root%\tests
+set SrcDir=%root%\tests
+
+rem Clear `srcs` and `objs` variables to store new source and object filenames
+set Srcs=
+set Objs=
+
+rem Set the `obj` and `bin` directories for the test project
+set ObjDir=%root%\tests\obj\win32\%config%_%platform%
+set BinDir=%root%\tests\bin\win32\%config%_%platform%
+
+:: Create required directories for test suite compilation
 if not exist tests\obj mkdir tests\obj
 if not exist tests\obj\win32\%config%_x86 mkdir tests\obj\win32\%config%_x86
 if not exist tests\obj\win32\%config%_x64 mkdir tests\obj\win32\%config%_x64
@@ -102,174 +177,147 @@ if not exist tests\bin mkdir tests\bin
 if not exist tests\bin\win32\%config%_x86 mkdir tests\bin\win32\%config%_x86
 if not exist tests\bin\win32\%config%_x64 mkdir tests\bin\win32\%config%_x64
 
-rem End of `:mkdirs` subroutine call
-goto :eof
+rem Compile test program and link library DLL
 
-rem ==========================
-rem Build
-rem ==========================
-:build
-rem Builds the entire solution
+rem Reference Platform and Win32 header files
+call :IncludeDirs %IncDir%
+if %ErrorLevel% neq 0 goto :Error
 
-rem Set the core library `include` and `src` directories
-set incdir=%root%\include
-set srcdir=%root%\src
-
-rem Set the core library `obj` and `bin` directories
-set objdir=%root%\obj\win32\%config%_%platform%
-set bindir=%root%\bin\win32\%config%_%platform%
-
-rem Clear variables to store source, include, and object filenames.
-set srcs=
-set incs=
-set objs=
-
-rem Recursively set all relative file paths of `*.h` header files
-for /r include %%F in (*.h) do (
-    call :include %%F
-)
-if %errorlevel% neq 0 goto :error
-
-rem Recursively set all relative file paths of `*.c` lib\logc files
-for /r lib\logc %%F in (*.h) do (
-    call :include %%F
-)
-if %errorlevel% neq 0 goto :error
-
-rem Recursively set all relative file paths of `*.c` source files
-for /r src %%F in (*.c) do ( 
-    call :sources %%F
-)
-if %errorlevel% neq 0 goto :error
-
-rem Recursively set all relative file paths of `*.c` lib\logc files
-for /r lib\logc %%F in (*.c) do ( 
-    call :sources %%F
-)
-if %errorlevel% neq 0 goto :error
-
-goto :compile
-
-rem ==========================
-rem Include
-rem ==========================
-:include
-rem Reference the absolute and relative paths of each `*.h` header file
-
-call :relpath %~dp1
-
-rem Prevent duplicate include directories
-if !prev! neq !relpath! (
-
-    call set "incs=%%incs%% -I%root%!relpath:~0,-1!"
-    call :addcompileflag !relpath!
-
-    set "prev=!relpath!"
-)
-
-rem End of :include subroutine call
-goto :eof
-
-:sources
-call :relpath %1
-call set "srcs=%%srcs%% %root%!relpath!"
-
-rem End of :sources subroutine call
-goto :eof
-
-rem ==========================
-rem Compile
-rem ==========================
-:compile
-rem Compile core library `*.c` files
-cl /c /MD -Zi -W4 -Wall /std:c17 /Fo%objdir%\ /Fd"%objdir%\vc140.pdb" %incs% %srcs%
-if %errorlevel% neq 0 goto :error
-echo Library source files compiled successfully & echo.
-
-rem Store all *.obj file names only for the linker
-for /r obj %%F in (*.obj) do (
-    call set "objs=%%objs%% %objdir%\%%~nxF"
-)
-
-rem Link *.obj object files
-LINK /nologo /DEBUG %objs:~1% /DLL /OUT:%bindir%\%_dll% /DEF:libcspd.defs
-if %errorlevel% neq 0 goto :error
-call :relpath %bindir%
-echo Library file `%_lib%` created in `%relpath%` successfully. & echo.
-
-rem Copy DLL to where the test executable will be built
-copy %bindir%\%_dll% %root%\tests\bin\win32\%config%_%platform% > nul
-
-rem ==========================
-rem Test Executable
-rem ==========================
-
-rem Set the core library `include` and `src` directories
-set incdir=%root%\tests
-set srcdir=%root%\tests
-
-rem Clear `srcs` and `objs` variables to store new source and object filenames
-set srcs=
-set objs=
-
-rem Set the `obj` and `bin` directories for the test project
-set objdir=%root%\tests\obj\win32\%config%_%platform%
-set bindir=%root%\tests\bin\win32\%config%_%platform%
-
-rem Store all `*.test.c` source files in a variable
-for /r tests %%F in (*.test.c) do ( 
-    call :relpath %%F
-    call set "srcs=%%srcs%% %root%!relpath!"
-)
-
-rem Compile test program and link DLL
+rem Reference Platform and Win32 source files
+call :SourceFiles %SrcDir%
+if %ErrorLevel% neq 0 goto :Error
 
 rem Compile *.test.c files
-cl /nologo /c /MD -Zi -W4 -Wall /std:c17 /Fo%objdir%\ /Fd"%objdir%\vc140.pdb" %incs% %srcs:~1%
-if %errorlevel% neq 0 goto :error
+cl /nologo /c /MD -Zi -W4 -Wall /std:c17 /Fo%ObjDir%\ /Fd"%ObjDir%\vc140.pdb" %Incs% %Srcs:~1%
+if %ErrorLevel% neq 0 goto :error
 echo Test source files compiled successfully & echo.
 
-rem Store all *.test.obj file names only for the linker
-for /r tests %%F in (*.test.obj) do (
-    call set "objs=%%objs%% %objdir%\%%~nxF"
-)
+call :ObjectFiles %ObjDir%
+if %ErrorLevel% neq 0 goto :Error
 
 rem Link *.test.obj object files
-LINK /DEBUG %objs:~1% /OUT:%bindir%\%_exe% %root%\bin\win32\Debug_x64\%_lib%
-if %errorlevel% neq 0 goto :error
+LINK /DEBUG %Objs:~1% /OUT:%BinDir%\%_exe% %Root%\bin\win32\Debug_x64\%_lib%
+if %ErrorLevel% neq 0 goto :error
 echo Test source files compiled successfully & echo.
 
 echo Build succeeded.
-goto :eof
+goto :EOF
 
-rem ==========================
-rem Relative Path
-rem ==========================
+
+::===========================================================================
+:: Helper Routines
+::===========================================================================
+
+
+::---------------------------------------------------------------------------
+:: Clean
+::---------------------------------------------------------------------------
+rem Deletes all intermediary and binary files generated by the previous build(s)
+:Clean
+
+del /S /Q bin\*.dll bin\*.exe bin\*.lib bin\*.pdb bin\*.ilk bin\*.exp > nul 2>&1
+del /S /Q obj\*.obj obj\*.pdb > nul 2>&1
+del /S /Q tests\obj\*.obj tests\obj\*.pdb > nul 2>&1 
+del /S /Q tests\bin\*.dll tests\bin\*.exe tests\bin\*.lib tests\bin\*.pdb^
+    tests\bin\*.ilk tests\bin\*.exp > nul 2>&1
+
+rem End of :Clean subroutine call
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Relative Path
+::-----------------------------------------------------------------------------
 rem Gets the relative path from the given absolute path
-:relpath
-set "abspath=%1%"
-set "relpath=!abspath:%root%=!"
-goto :eof
+:RelativePath
 
-rem ==========================
-rem Compile Flags
-rem ==========================
-:addcompileflag
-rem Adds a compile flag to `compile_flags.txt` for the Clang LSP.
-set flag=%1
+set "AbsPath=%1%"
+set "RelPath=!AbsPath:%Root%=!"
+
+rem End of :RelativePath subroutine call
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Add Compile Flags
+::-----------------------------------------------------------------------------
+rem Adds a compile flag to `compile_flags.txt` for the Clang LSP
+:AddCompileFlag
+
+set Flag=%1
+
 rem Invert slashes for include paths to be compatible with the LSP
-set cpath=!flag:\=/!
-echo -I>> %root%\%flags%
-echo .!cpath!>> %root%\%flags%
-goto :eof
+set FilePath=!Flag:\=/!
 
-rem ==========================
-rem Error
-rem ==========================
-:error
+rem Write flag to `compile_flags.txt` file
+echo -I>> %Root%\%CompileFlags%
+echo .!FilePath!>> %Root%\%CompileFlags%
+
+rem End of :AddCompileFlag subroutine call
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Include Files
+::-----------------------------------------------------------------------------
+rem Reference the absolute and relative paths of each `*.h` header file
+:IncludeDirs
+
+for /r %1 %%A in (*.h) do (
+
+    call :RelativePath %%~dpA
+
+    rem Prevent duplicate include directories
+    if !Prev! neq !RelPath! (
+
+        call set "Incs=%%Incs%% -I%Root%!RelPath:~0,-1!"
+        call :AddCompileFlag !RelPath!
+
+        set "Prev=!RelPath!"
+    )
+)
+
+rem End of :IncludeDirs subroutine call
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Source Files
+::-----------------------------------------------------------------------------
+rem Reference source files to be compiled
+:SourceFiles
+
+for /r %1 %%I in (*.c) do (
+    call set "Srcs=%%Srcs%% %%~fI"
+)
+
+rem End of :SourceFiles subroutine call
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Object Files
+::-----------------------------------------------------------------------------
+rem Reference intermediary object files
+:ObjectFiles
+
+for /r %1 %%I in (*.obj) do (
+    call set "Objs=%%Objs%% %1\%%~nxI"
+)
+
+rem End of :ObjectFiles subroutine call
+goto :EOF
+
+
+::-----------------------------------------------------------------------------
+:: Error
+::-----------------------------------------------------------------------------
 rem Error function to call when a build error is encountered
-set err=%errorlevel%
+:Error
+
+set Err=%ErrorLevel%
 echo. 
 echo Build failed.
-echo Exited with error code: %errorlevel%
-exit /b %errorlevel%
+echo Exited with error code: %ErrorLevel%
+
+exit /b %ErrorLevel%
